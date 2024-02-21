32a86fd71bb50164a4bac972fb884e17821972c9
hasanBafuleh <hasanbafuleh@gmail.com>
2024-02-21 18:47:32 +0300

    minor change to force push origin

--- utils/database.js
+++ utils/database.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-let isConnected = false; //track the connection status important
+let isConnected = false;
 
 export const connectToDB = async () => {
   mongoose.set("strictQuery", true);
