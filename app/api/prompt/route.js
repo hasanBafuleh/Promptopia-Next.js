11f7bdad25c3de4c448810e3ad3d50935131967f
hasanBafuleh <hasanbafuleh@gmail.com>
2024-02-25 00:26:04 +0300

    Trying to solve the realtime feed update

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -7,6 +7,9 @@ export const GET = async (request) => {
 
     const prompts = await Prompt.find({}).populate("creator");
 
+        // Setting Cache-Control header to 'no-store' to prevent caching
+        const headers = new Headers({ 'Cache-Control': 'no-store' });
+
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
