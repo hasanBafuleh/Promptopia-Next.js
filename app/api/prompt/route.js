3561e504dfce64db5198f1aa2d493254c883d7c5
hasanBafuleh <hasanbafuleh@gmail.com>
2024-02-26 19:52:24 +0300

    undoing a lot of code

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -2,22 +2,13 @@ import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
 export const GET = async (request) => {
-  try {
-    await connectToDB();
+    try {
+        await connectToDB()
 
-    const prompts = await Prompt.find({}).populate("creator");
+        const prompts = await Prompt.find({}).populate('creator')
 
-    // Set appropriate cache control headers
-    const responseHeaders = {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store", // This disables caching
-    };
-
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: responseHeaders,
-    });
-  } catch (error) {
-    return new Response("Failed to fetch all prompts", { status: 500 });
-  }
-};
+        return new Response(JSON.stringify(prompts), { status: 200 })
+    } catch (error) {
+        return new Response("Failed to fetch all prompts", { status: 500 })
+    }
+} 
