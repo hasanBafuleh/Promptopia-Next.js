9d4a63a773ef14ef2d671d2b9b94d5244f727382
hasanBafuleh <hasanbafuleh@gmail.com>
2024-02-27 18:32:24 +0300

    adding revalidate = 0

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,19 +1,15 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
+export const revalidate = 0;
+
 export const GET = async (request) => {
   try {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate("creator");
 
-    return new Response(JSON.stringify(prompts), {
-      status: 200,
-      headers: {
-        "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
-        "Content-Type": "application/json",
-      },
-    });
+    return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all prompts", { status: 500 });
   }
